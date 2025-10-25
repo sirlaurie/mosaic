78edf20d63f3cd9bc929c8176e60f12a36ec18f6
rorycheung <mr.rorycheung@gmail.com>
2025-10-25 22:16:16 +1100

    Initial Commit

---
+++ Mosaic/MosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicApp.swift
+//  Mosaic
+//
+//  Created by lori on 2025/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
